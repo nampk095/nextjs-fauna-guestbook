7192c063f132e04838f46052abc1207a51c9fafc
MinhQuy <nampk095@gmail.com>
2021-11-20 16:40:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -115,10 +115,10 @@ const EntryForm = ({ onSubmit: onSubmitProp }) => {
         />
         <button
           className={cn(
-            'flex items-center justify-center',
+            'flexxx items-center justify-center',
             'absolute right-1 top-1 px-4 font-bold h-8',
-            'bg-gray-100 dark:bg-gray-700 text-gray-900',
-            'dark:text-gray-100 rounded w-28'
+            'bg-blue-100 dark:bg-blue-700 text-blue-900',
+            'dark:text-blue-100 rounded w-28'
           )}
           type="submit"
           disabled={isSubmitting}
